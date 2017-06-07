413c9b1e4ef23925fe81b63078c3f3186c4b272f
poltts <poltss@gmail.com>
2017-06-07 16:53:26 -0300

    update server

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app        = express();
 const helmet     = require('helmet');
 const logger     = require('morgan'); 
 
-const port       = 8080;
+const port       = process.env.PORT || 8080;
 const uri        = 'http: //localhost: ' + port; 
 // const api        = require('./routes/index');
 
@@ -22,11 +22,11 @@ app.use(express.static(__dirname));
 // app.use('api',api);
 app.use((req, res) => res.sendFile(`${__dirname}/index.html`));
 
-app.use((err, request, response, next) => {  
-  // log the error, for now just console.log
-  console.log(err)
-  response.status(500).send('Something broke!')
-});
+// app.use((err, request, response, next) => {  
+//   // log the error, for now just console.log
+//   console.log(err)
+//   response.status(500).send('Something broke!')
+// });
 
 app.listen(port, function(){
     console.log('sever on ' + uri);
