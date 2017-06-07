9c45b318a1ddeafe688343dad5381e95eb3d18a3
poltts <poltss@gmail.com>
2017-06-07 13:31:35 -0300

    add video component and video img

--- dist/js/global.js
+++ dist/js/global.js
@@ -108,6 +108,24 @@ web.directive('userProfile', function() {
     templateUrl: '../../../views/templates/user-profile.html'
   }; 
 });
+ 
+/* DIRECTIVE   */
+web.directive('videos', function() { 
+  return { 
+    restrict: 'AE', 
+    scope: { 
+      open: '=',
+      info: '='
+    }, 
+    link: function(scope,el,attr){
+        if(scope.open == 0){
+            //closed
+            el.attr.class = '.closed';
+        }
+    },
+    templateUrl: '../../../views/templates/videos.html'
+  }; 
+});
 /* HOME CONTROLLER  */
  web.controller('homeController',['$scope', '$location', '$route', '$routeParams', 
  function ($scope, $location, $route, $routeParams) {
@@ -139,5 +157,56 @@ web.directive('userProfile', function() {
 		 userName: 'Marina Mota',
 		 userPic: 'http://localhost:8080/dist/img/user-pic.png'
 	 }; 
+	 $scope.videoData = [
+		{
+			id: 1,
+			type: 'video',
+			time: '7:21',
+			category: 'Assistêncioa pré-natal',
+			title: 'Alterações no sistema nervoso  do recém-na...',
+			img: 'dist/img/01.png',
+			rate: 3
+		},
+		{
+			id: 2,
+			type: 'video',
+			time: '37:20',
+			category: 'Climatério',
+			title: 'Hiportemia terapêutica na asfixia neonatal',
+			img: 'dist/img/02.png',
+			rate: 2
+		},
+		{
+			id: 3,
+			type: 'simulado',
+			questions: 30,
+			category: 'Trombofilias',
+			title: 'Simulado sobre trombofilias',
+			img: 'dist/img/03.png',
+			rate: 3
+		},
+		{
+			id: 4,
+			type: 'video',
+			time: '3:15',
+			category: 'Trombofilias',
+			title: 'Adenomegalias febris na adolescência',
+			img: 'dist/img/04.png'
+		},
+		{
+			id: 5,
+			type: 'simulado',
+			questions: 40,
+			category: 'Assistêncioa pré-natal',
+			title: 'Simulado sobrecontagem dinâmica',
+			img: 'dist/img/05.png'
+		}
+	 ];
+	 $scope.videoComplete = '20,1%';
+
+	 $scope.videos = {
+		 data: $scope.videoData,
+		 progress: $scope.videoComplete
+	 }
 
 }]);  
